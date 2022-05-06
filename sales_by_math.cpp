f864bf68e1b16f5abf4b9eb8f43c34cc35cf0d3c
mcflinta <21521901@gm.uit.edu.vn>
2022-05-06 14:32:23 +0700

    update sale_by_math.cpp

--- sales_by_math.cpp
+++ sales_by_math.cpp
@@ -20,23 +20,22 @@ void insert_sort(int n, int a[])
 int in_pair(int n, int a[])
 {
     int sum = 0;
-    for(int i = 0; i < n; i+= 2)
+    for(int i = 0; i < n; i += 2)
     {
         if(a[i] == a[i + 1])
-            sum++;     
+            sum++;  
+        else
+            i--;   
     }
     return sum;
 }
 int main()
 {
-    int n, a[100];
+    int n, a[150];
     cin >> n;
     for(int i = 0; i <n; i++)
         cin >> a[i];
     insert_sort(n, a);
     cout << in_pair(n, a);
-    cout << endl;
-    for(int i = 0; i < n; i++)
-        cout << a[i] << " ";
     return 0;
 }
